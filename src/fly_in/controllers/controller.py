ca5dffd0425399fab3ca19cb37f8d79548ee1a03
Anselme Bounouar <abounoua@student.42lyon.fr>
2026-05-26 17:42:21 +0200

    refactor controllers: extract orchestration logic from the view layer

---
+++ src/fly_in/controllers/controller.py
@@ -0,0 +1,19 @@
+import pyray as pr
+
+
+from fly_in.models.world import WorldModel
+from fly_in.view.world import WorldView
+
+class ControllerError(Exception):
+    pass
+
+
+class WorldController:
+    def __init__(self, view: WorldView, model: WorldModel) -> None:
+        self.view = view
+        self.model = model
+
+    def loop(self):
+        while not pr.window_should_close():
+            self.model.update_state()
+            self.view.render(self.model)
