7f4b9a758e31dea6f270e531966bdb8b8a6a07d3
Anselme Bounouar <abounoua@student.42lyon.fr>
2026-05-26 17:42:14 +0200

    feat models: implement pure logic entities for hubs and connections

---
+++ src/fly_in/models/connection.py
@@ -0,0 +1,15 @@
+import pyray as pr
+
+
+class ConnectionModel:
+    def __init__(
+        self,
+        start: pr.Vector3,
+        end: pr.Vector3,
+        color: pr.Color = pr.BLACK
+    ) -> None:
+        self.start = start
+        self.end = end
+        self.start.y += 1
+        self.end.y += 1
+        self.color = color
---
+++ src/fly_in/models/hub.py
@@ -0,0 +1,18 @@
+import pyray as pr
+
+
+class HubModel:
+    def __init__(
+        self,
+        position: pr.Vector3,
+        color: pr.Color,
+        name: str,
+        radius: float = 3,
+        height: float = 2,
+        slices: int = 8
+    ) -> None:
+        self.position = position
+        self.radius = radius
+        self.color = color
+        self.height = height
+        self.slices = slices
