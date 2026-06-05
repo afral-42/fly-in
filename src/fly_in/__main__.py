bfd10da72d99e5a739b8cda1a487864939497cd1
Anselme <anselme.bounouar.42@gmail.com>
2026-06-05 11:33:44 +0200

    feat: restricted, priority, connection restrictions and blocked handling

--- src/fly_in/__main__.py
+++ src/fly_in/__main__.py
@@ -1,59 +1,101 @@
 from collections import deque
-import random
+
+import pyray as pr
 
 from fly_in.controllers.controller import WorldController
 from fly_in.models.world import WorldModel
 from fly_in.parsing import ConfigParser, ParsingError
-import pyray as pr
-
-from fly_in.schemas import FlyinConfig
+from fly_in.schemas import FlyinConfig, ZoneType
 from fly_in.services.dijkstra import Position, ReservedDijkstra
 from fly_in.view.world import WorldView
 
 
 def build_graph(config: FlyinConfig) -> dict:
-    # 1. On crée un dictionnaire rapide pour retrouver les Positions grâce au nom du hub
-    # (On inclut bien tous les hubs, y compris start et end pour être sûr)
     all_hubs = config.hubs + [config.start_hub, config.end_hub]
     positions_by_name = {
-        hub.name: Position(hub.x, hub.y, hub.name) 
+        hub.name: Position(
+            hub.x,
+            hub.y,
+            hub.name,
+            hub.metadatas.zone == ZoneType.RESTRICTED,
+            is_priority=hub.metadatas.zone == ZoneType.PRIORITY,
+            is_blocked=hub.metadatas.zone == ZoneType.BLOCKED,
+        )
         for hub in all_hubs
     }
 
-    # 2. On initialise le graphe avec des listes vides pour chaque Position
     graph = {pos: [] for pos in positions_by_name.values()}
 
-    # 3. On lit les connexions et on branche les tuyaux !
     for connection in config.connections:
-        if connection.start_name in positions_by_name and connection.end_name in positions_by_name:
+        if (
+            connection.start_name in positions_by_name
+            and connection.end_name in positions_by_name
+        ):
             start_pos = positions_by_name[connection.start_name]
             end_pos = positions_by_name[connection.end_name]
-            
-            # On ajoute le voisin à la liste du hub de départ
-            graph[start_pos].append(end_pos)
-            graph[end_pos].append(start_pos)
+
+            if start_pos.is_blocked or end_pos.is_blocked:
+                continue
+            real_edge = (start_pos.hub_name, end_pos.hub_name)
+
+            if end_pos.is_restricted:
+                middle_point = Position(
+                    (start_pos.x + end_pos.x) / 2,
+                    (start_pos.y + end_pos.y) / 2,
+                    f"{start_pos.hub_name}-{end_pos.hub_name}",
+                    False,
+                    True,
+                    real_edge,
+                )
+                if middle_point not in graph:
+                    graph[middle_point] = []
+                graph[start_pos].append(middle_point)
+                graph[middle_point].append(end_pos)
+            else:
+                graph[start_pos].append(end_pos)
+
+            if start_pos.is_restricted:
+                middle_point = Position(
+                    (start_pos.x + end_pos.x) / 2,
+                    (start_pos.y + end_pos.y) / 2,
+                    f"{end_pos.hub_name}-{start_pos.hub_name}",
+                    False,
+                    True,
+                    real_edge,
+                )
+                if middle_point not in graph:
+                    graph[middle_point] = []
+                graph[end_pos].append(middle_point)
+                graph[middle_point].append(start_pos)
+            else:
+                graph[end_pos].append(start_pos)
 
     return graph
+
 
 def build_restrictions(graph: dict, config: FlyinConfig) -> dict:
     restrictions = {}
     for hub in config.hubs:
-        restrictions[Position(hub.x, hub.y, hub.name)] = hub.metadatas.max_drones
-    
+        restrictions[Position(hub.x, hub.y, hub.name)] = (
+            hub.metadatas.max_drones
+        )
+
     return restrictions
+
 
 def build_connections(config: FlyinConfig) -> dict:
     restrictions = {}
     for connection in config.connections:
-        restrictions[
-            (connection.start_name, connection.end_name)
-        ] = connection.metadatas.max_link_capacity
+        restrictions[(connection.start_name, connection.end_name)] = (
+            connection.metadatas.max_link_capacity
+        )
 
     return restrictions
 
 
 def get_pr_color(color: str) -> pr.Color:
     return getattr(pr, color.upper())
+
 
 def build_world(config: FlyinConfig) -> WorldModel:
     model = WorldModel()
@@ -62,12 +104,14 @@ def build_world(config: FlyinConfig) -> WorldModel:
     model.add_hub(
         config.start_hub.name,
         get_pr_color(config.start_hub.metadatas.color),
-        pr.Vector3(config.start_hub.x * SCALE, 0.0, config.start_hub.y * SCALE)
+        pr.Vector3(
+            config.start_hub.x * SCALE, 0.0, config.start_hub.y * SCALE
+        ),
     )
     model.add_hub(
         config.end_hub.name,
         get_pr_color(config.end_hub.metadatas.color),
-        pr.Vector3(config.end_hub.x * SCALE, 0.0, config.end_hub.y * SCALE)
+        pr.Vector3(config.end_hub.x * SCALE, 0.0, config.end_hub.y * SCALE),
     )
 
     for hub in config.hubs:
@@ -75,46 +119,48 @@ def build_world(config: FlyinConfig) -> WorldModel:
             hub.name,
             get_pr_color(hub.metadatas.color),
             pr.Vector3(hub.x * SCALE, 0.0, hub.y * SCALE),
-            hub.metadatas.max_drones
+            hub.metadatas.max_drones,
         )
         model.add_text(
             str(hub.metadatas.max_drones),
             pr.Vector3(hub.x * SCALE, 2.1, hub.y * SCALE),
             3.0,
-            get_pr_color(hub.metadatas.color)
+            get_pr_color(hub.metadatas.color),
         )
 
     all_hubs = config.hubs + [config.start_hub, config.end_hub]
     hubs_by_name = {hub.name: hub for hub in all_hubs}
     for connection in config.connections:
-        model.add_connection(
-            connection.start_name,
-            connection.end_name
-        )
+        model.add_connection(connection.start_name, connection.end_name)
         start_hub = hubs_by_name[connection.start_name]
         end_hub = hubs_by_name[connection.end_name]
 
         model.add_text(
             str(connection.metadatas.max_link_capacity),
-            pr.Vector3(((start_hub.x + end_hub.x) / 2) * SCALE, 1.5, ((start_hub.y + end_hub.y) / 2) * SCALE),
+            pr.Vector3(
+                ((start_hub.x + end_hub.x) / 2) * SCALE,
+                1.5,
+                ((start_hub.y + end_hub.y) / 2) * SCALE,
+            ),
             3.0,
+            pr.LIGHTGRAY,
             pr.BLACK,
-            pr.WHITE,
-
         )
 
-    # Test de notre djikstra
     dijkstra = ReservedDijkstra(
         build_graph(config),
-        Position(config.start_hub.x, config.start_hub.y, config.start_hub.name),
+        Position(
+            config.start_hub.x, config.start_hub.y, config.start_hub.name
+        ),
         Position(config.end_hub.x, config.end_hub.y, config.end_hub.name),
         build_restrictions(build_graph(config), config),
-        build_connections(config)
+        build_connections(config),
     )
 
     for i in range(config.nb_drones):
         path = []
         solved = dijkstra.solve()
+
         if solved is None:
             raise Exception
         for position in solved:
@@ -122,7 +168,9 @@ def build_world(config: FlyinConfig) -> WorldModel:
 
         model.add_drone(
             deque(path),
-            pr.Vector3(config.start_hub.x * SCALE, 3, config.start_hub.y * SCALE),
+            pr.Vector3(
+                config.start_hub.x * SCALE, 3, config.start_hub.y * SCALE
+            ),
         )
         dijkstra.reset()
 
@@ -131,7 +179,7 @@ def build_world(config: FlyinConfig) -> WorldModel:
 
 def main() -> None:
     try:
-        parser = ConfigParser("maps/hard/01_maze_nightmare.txt")
+        parser = ConfigParser("maps/challenger/01_the_impossible_dream.txt")
         config = parser.parse()
 
         print(config.model_dump_json(indent=4))
--- src/fly_in/services/dijkstra.py
+++ src/fly_in/services/dijkstra.py
@@ -2,10 +2,25 @@ import heapq
 
 
 class Position:
-    def __init__(self, x: int, y: int, hub_name: str) -> None:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        hub_name: str,
+        is_restricted: bool = False,
+        is_flight_node: bool = False,
+        physical_connection: tuple[str, str] | None = None,
+        is_priority: bool = False,
+        is_blocked: bool = False,
+    ) -> None:
         self.x = x
         self.y = y
         self.hub_name = hub_name
+        self.is_restricted = is_restricted
+        self.is_flight_node = is_flight_node
+        self.physical_connection = physical_connection
+        self.is_priority = is_priority
+        self.is_blocked = is_blocked
 
     def __hash__(self) -> int:
         return hash(self.hub_name)
@@ -16,6 +31,10 @@ class Position:
         return self.hub_name == other.hub_name
 
     def __lt__(self, other: Position) -> bool:
+        if self.is_priority and not other.is_priority:
+            return True
+        elif not self.is_priority and other.is_priority:
+            return False
         return self.hub_name < other.hub_name
 
 
@@ -26,9 +45,11 @@ class ReservedDijkstra:
         start: Position,
         end: Position,
         hub_restrictions: dict[Position, int],
-        connection_restrictions: dict[tuple[str, str], int]
+        connection_restrictions: dict[tuple[str, str], int],
     ) -> None:
-        self.connection_reservation: dict[tuple[tuple[str, str], int], int] = {}
+        self.connection_reservation: dict[
+            tuple[tuple[str, str], int], int
+        ] = {}
         self.hub_reservation: dict[tuple[Position, int], int] = {}
         self.costs: list[tuple[int, Position]] = []
         self.graph = graph
@@ -46,7 +67,6 @@ class ReservedDijkstra:
     def normalize_connection(connection: tuple[str, str]) -> tuple[str, str]:
         return min(connection), max(connection)
 
-
     def is_full(self, position: Position, turn: int) -> bool:
         reserved = self.hub_reservation.get((position, turn))
         restriction = self.hub_restrictions.get(position)
@@ -56,8 +76,20 @@ class ReservedDijkstra:
 
         return reserved >= restriction
 
-    def is_full_connection(self, position: Position, next: Position, turn: int) -> bool:
-        connection = self.normalize_connection((position.hub_name, next.hub_name))
+    def is_full_connection(
+        self, position: Position, next: Position, turn: int
+    ) -> bool:
+        if position.is_flight_node and position.physical_connection:
+            connection = self.normalize_connection(
+                position.physical_connection
+            )
+        elif next.is_flight_node and next.physical_connection:
+            connection = self.normalize_connection(next.physical_connection)
+        else:
+            connection = self.normalize_connection(
+                (position.hub_name, next.hub_name)
+            )
+
         reserved = self.connection_reservation.get((connection, turn))
         restriction = self.connection_restrictions.get(connection)
 
@@ -66,22 +98,22 @@ class ReservedDijkstra:
 
         return reserved >= restriction
 
-
     def push_neighbors(self, position: Position, cost: int) -> None:
         next_turn = cost + 1
 
         for next in self.graph[position]:
             if (
-                not self.is_full(next, next_turn) and
-                not self.is_full_connection(position, next, next_turn) and
-                (next, next_turn) not in self.came_from
+                not self.is_full(next, next_turn)
+                and not self.is_full_connection(position, next, cost)
+                and (next, next_turn) not in self.came_from
             ):
                 heapq.heappush(self.costs, (next_turn, next))
                 self.came_from[(next, next_turn)] = position
 
         if (
-            not self.is_full(position, next_turn) and
-            (position, next_turn) not in self.came_from
+            not position.is_flight_node
+            and not self.is_full(position, next_turn)
+            and (position, next_turn) not in self.came_from
         ):
             heapq.heappush(self.costs, (next_turn, position))
             self.came_from[(position, next_turn)] = position
@@ -94,21 +126,43 @@ class ReservedDijkstra:
             if pos == self.end:
                 return self.compute_path(cost)
 
+            if cost > 1000:
+                return None
+
             self.push_neighbors(pos, cost)
 
     def add_reservations(self, path: list[Position]) -> None:
-        for i in range(0, len(path) - 1):
-            prev, next = path[i].hub_name, path[i + 1].hub_name
-            connection = self.normalize_connection((prev, next))
-            if (connection, i) not in self.connection_reservation:
-                self.connection_reservation[(connection, i)] = 0
-            self.connection_reservation[(connection, i)] += 1
-
-        for i, pos in enumerate(path, 1):
+        path.insert(0, self.start)
+        for i, pos in enumerate(path, 0):
             if (pos, i) not in self.hub_reservation:
                 self.hub_reservation[(pos, i)] = 0
             self.hub_reservation[(pos, i)] += 1
 
+            if i < len(path) - 1:
+                next_pos = path[i + 1]
+
+                if pos == next_pos:
+                    continue
+
+                if pos.is_flight_node and pos.physical_connection:
+                    connection = self.normalize_connection(
+                        pos.physical_connection
+                    )
+                elif next_pos.is_flight_node and next_pos.physical_connection:
+                    connection = self.normalize_connection(
+                        next_pos.physical_connection
+                    )
+                else:
+                    connection = self.normalize_connection(
+                        (pos.hub_name, next_pos.hub_name)
+                    )
+
+                if (connection, i) not in self.connection_reservation:
+                    self.connection_reservation[connection, i] = 0
+
+                self.connection_reservation[(connection, i)] += 1
+
+        path.pop(0)
 
     def reset(self) -> None:
         self.costs = []
@@ -118,11 +172,12 @@ class ReservedDijkstra:
         path: list[Position] = []
         pos = self.end
 
-        while (cost):
+        while cost:
             path.append(pos)
             pos = self.came_from[pos, cost]
             cost -= 1
 
         path.reverse()
+        print([p.hub_name for p in path])
         self.add_reservations(path)
         return path
